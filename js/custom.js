5e720e295912049cc12dd19df1d20c3794791a42
Ben Zhao <96789119+bzhao-1@users.noreply.github.com>
2022-11-28 14:03:48 -0500

    Javascript files

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+
+/*----------- CUSTOM JS SCRIPTS -----------*/
+
+(function($) {
+  'use strict';
+  $(function() {
+    // Code here executes When the DOM is loaded...
+  });
+  $(window).on('load', function() {
+    // Code here executes When the page is loaded
+  });
+}(jQuery));
